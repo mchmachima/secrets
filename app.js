bb40c47c5c3fecde5033c07703e1e7f441b9d37e
Machchima J <mmchmachima@gmail.com>
2023-08-07 00:05:00 +0700

    Level 6 OAuth 2.0 and Implement Sign In with Google

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 /////////////////////////////////////////////////////// Setting ///////////////////////////////////////////////////////
 //jshint esversion:6
-// require("dotenv").config();
+require("dotenv").config();
 // console.log(process.env);
 const express = require("express");
 const bodyParser = require("body-parser");
@@ -9,6 +9,8 @@ const mongoose = require("mongoose");
 const session = require("express-session");
 const passport = require("passport");
 const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const findOrCreate = require("mongoose-findorcreate");
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -31,21 +33,67 @@ mongoose.connect("mongodb://127.0.0.1:27017/userDB");
 const userSchema = new mongoose.Schema({
   email: String,
   password: String,
+  googleId: String,
+  secret: String,
 });
 
 userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
+
 const User = new mongoose.model("User", userSchema);
+
 // Add below after created model
 // Use passport-local-mongoose to create local login
 passport.use(User.createStrategy());
 
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+passport.serializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, {
+      id: user.id,
+      username: user.username,
+    });
+  });
+});
+
+passport.deserializeUser(function (user, cb) {
+  process.nextTick(function () {
+    return cb(null, user);
+  });
+});
+// Should add below serialize and deserialize
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "http://localhost:3000/auth/google/secrets",
+      userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo",
+    },
+    function (accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ googleId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
+  )
+);
 /////////////////////////////////////////////////////// Home route ///////////////////////////////////////////////////////
 app.route("/").get((req, res) => {
   res.render("home");
 });
 
+/////////////////////////////////////////////////////// Google route ///////////////////////////////////////////////////////
+app
+  .route("/auth/google")
+  .get(passport.authenticate("google", { scope: ["profile"] })); // Profile is user id on Google to identify
+
+app.get(
+  "/auth/google/secrets",
+  passport.authenticate("google", { failureRedirect: "/login" }),
+  function (req, res) {
+    // Successful authentication, redirect secrets route.
+    res.redirect("/secrets");
+  }
+);
 /////////////////////////////////////////////////////// Register route ///////////////////////////////////////////////////////
 app
   .route("/register")
@@ -94,11 +142,14 @@ app
 
 /////////////////////////////////////////////////////// Secrets route ///////////////////////////////////////////////////////
 app.route("/secrets").get((req, res) => {
-  if (req.isAuthenticated()) {
-    res.render("secrets");
-  } else {
-    res.redirect("/login");
-  }
+  User.find({ secret: { $ne: null } })    // Find all data have secrets
+    .then(foundUser => {
+      if (foundUser) {
+        res.render("secrets", {userWithSecrets: foundUser})
+      } else {
+        console.log(err);
+      }
+    });
 });
 
 /////////////////////////////////////////////////////// Logout route ///////////////////////////////////////////////////////
@@ -107,11 +158,37 @@ app.route("/logout").get((req, res) => {
     if (err) {
       console.log(err);
     } else {
-      res.redirect("/")
+      res.redirect("/");
     }
-  })
+  });
 });
 
+/////////////////////////////////////////////////////// Submit route ///////////////////////////////////////////////////////
+app
+  .route("/submit")
+  .get((req, res) => {
+    if (req.isAuthenticated()) {
+      res.render("submit");
+    } else {
+      res.redirect("/login");
+    }
+  })
+  .post((req, res) => {
+    if (req.isAuthenticated()) {
+      const submittedSecret = req.body.secret;
+      User.findById(req.user.id).then((foundUser) => {
+        if (foundUser) {
+          foundUser.secret = submittedSecret;
+          foundUser.save().then(() => {
+            console.log("Successfully add secrets!");
+            res.redirect("/secrets");
+          });
+        } else {
+          console.log(err);
+        }
+      });
+    }
+  });
 /////////////////////////////////////////////////////// Listen route ///////////////////////////////////////////////////////
 app.listen(3000, function () {
   console.log("Server started on port 3000");
